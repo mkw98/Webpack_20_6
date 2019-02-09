aa46ce305a7d061cdb18cee417771b919e875e39
Maria Wieckowska <mkw98@gazeta.pl>
2019-02-09 22:49:37 +0100

    init project

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Title = props =>
+    <div>
+      <h1>{props.title}</h1>
+      <p>To do:</p>
+    </div>
+
+export default Title;
+
+/// function Title(props) {
+//    return (
+//        <h1>{props.title}</h1>
+ //   )
+//}
